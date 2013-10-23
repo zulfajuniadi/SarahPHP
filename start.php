cd58112cee9a20dbb8d3b47c45060c162161a08f
Zulfa Juniadi <zulfajuniadi@gmail.com>
2013-10-24 01:29:19 +0800

    Added start.php

--- start.php
+++ start.php
@@ -1,1 +1,55 @@
 <?php
+
+/* Setup Default Timezone */
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
+
+/* Set Environment Variables */
+
+$env = [
+    'development' => 'sarahphp.dev',
+    'staging'     => 'sarahphp.stg',
+    'production'  => 'sarahphp.com'
+];
+
+foreach ($env as $name => $hostname) {
+    Environment::add($name, function() use ($hostname) {
+        return stristr(BASEURL, $hostname) !== false; 
+    });
+}
+
+/* Setup the application based on the detected environment */
+
+Environment::is('development', function(){
+    
+    Model::setConnection('sqlite:../resource/database/development.db');
+    error_reporting(E_ALL);
+
+});
+
+Environment::is('staging', function(){
+
+    Model::setConnection('sqlite:../resource/database/development.db');
+    error_reporting(E_ERROR);
+
+});
+
+Environment::is('production', function(){
+
+    Model::setConnection('sqlite:../resource/database/production.db');
+    error_reporting(0);
+
+});
+
+
+/* Setup the application Models */
+
+/*
+
+    class Tasks extends Model
+    {
+        static $tableName = 'tasks';
+    }
+
+
+ */
