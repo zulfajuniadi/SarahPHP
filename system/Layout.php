9db991f4b5fd89f43379d1236260d5341ae9756c
Zulfa Juniadi <zulfajuniadi@gmail.com>
2013-10-24 01:30:34 +0800

    WIP : Layouts

--- system/Layout.php
+++ system/Layout.php
@@ -13,8 +13,20 @@ class Layout
 
         if(!file_exists($layoutPath))
             throw new Exception("File {$layoutPath} does not exists." , 1);
-            
 
+        $layoutPartials = [];
+            
+        foreach ($partials as $path) {
+            $paths = array_merge(self::$layoutsDir, ['views'],  explode('.', $path));
+            $viewPath = join(DIRECTORY_SEPARATOR, $paths) . '.php';
+
+            if(!file_exists($viewPath))
+                throw new Exception("File {$viewPath} does not exists." , 1);
+
+            $layoutPartials[] = $viewPath;
+        }
+
+        self::$layouts[$layoutPath] = $layoutPartials;
 
         return print_r($paths, true);
     }
